f456ae9826b2dbac4c3f7c58051435dc10b2c401
Marcus Campbell <marcus.anthony.campbell@gmail.com>
2014-12-17 13:37:44 +0100

    .

--- run.sh
+++ run.sh
@@ -1,13 +1,10 @@
 #!/bin/bash 
 
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
 then
-    echo "Please supply the root directory. it's usually '.'"
-    echo "Please set env e.g. home, work"
+    echo "Please ser role e.g. pc, laptop"
     exit 1
 fi
 
-root=$1
-
-FACTER_role=$2 puppet apply /etc/puppet/manifests/site.pp
+FACTER_role=$1 puppet apply /etc/puppet/manifests/site.pp
 #FACTER_role=$2 puppet apply --verbose --debug /etc/puppet/manifests/site.pp
