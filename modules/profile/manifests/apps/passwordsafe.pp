0077fec741954cd631956a11a75c3e470d8b4308
Marcus Campbell <marcus.anthony.campbell@gmail.com>
2014-12-17 13:58:23 +0100

    .

--- modules/profile/manifests/apps/passwordsafe.pp
+++
@@ -1,8 +0,0 @@
-#
-class profile::apps::passwordsafe {
-  package{'passwordsafe': ensure => present }
-  # file{'/home/mcampbell/.??':
-  #   ensure => file,
-  #   source => 'puppet:///modules/pc/passwordsafe??'
-  # }
-}
