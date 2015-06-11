81c41647b84f906079d88b7994ce85035d01405e
Marcus Campbell <marcus.anthony.campbell@gmail.com>
2015-06-11 11:31:00 +0200

    move mount points

--- modules/profile/manifests/user/root.pp
+++ modules/profile/manifests/user/root.pp
@@ -1,10 +1,15 @@
 # This will set up all the configuration we want for the root user
 class profile::user::root (
-  $config = undef,
+  $dns_search = hiera('dns_search', ''),
   $colorscheme = hiera('vim::colorscheme', 'ron')
 ) {
+  $config = hiera_hash(profile::user::root::config)
+  $mnt_config = hiera_hash(profile::user::root::mnt_config, '')
   if $config {
     create_resources( file, $config )
+  }
+  if $mnt_config {
+    create_resources( mount, $mnt_config )
   }
 
   # All my vim goodness
@@ -12,4 +17,10 @@ class profile::user::root (
     ensure  => file,
     content => template('profile/vimrc.erb')
   }
+
+  file { '/etc/resolv.conf':
+    ensure  => file,
+    content => template('profile/resolv.conf.erb'),
+  }
+
 }
