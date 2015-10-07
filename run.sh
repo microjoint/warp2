5ae1782138c877df55cc3f4fc5a1a7b77a3bc297
Marcus Campbell <marcus.anthony.campbell@gmail.com>
2015-10-07 10:43:28 +0200

    new work DNS servers. changed vim colorscheme. more known hosts

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ then
   exit 1
 fi
 
-FACTER_environment=$1 FACTER_hardware=$2 FACTER_role=$3 puppet apply /etc/puppet/manifests/site.pp
+FACTER_environment=$1 FACTER_hardware=$2 FACTER_role=$3 puppet apply /etc/puppet/manifests/site.pp --debug --verbose
