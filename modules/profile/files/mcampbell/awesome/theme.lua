def824c29fb11bcb0ea4d00a496988b6cbeb05f3
Marcus Campbell <marcus.anthony.campbell@gmail.com>
2015-10-12 10:41:46 +0200

    path changes

--- modules/profile/files/mcampbell/awesome/theme.lua
+++ modules/profile/files/mcampbell/awesome/theme.lua
@@ -73,7 +73,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper_cmd = { "awsetbg /home/mcampbell/sync/Dropbox/1-data/2-images/backdrops/thematrix.jpg" }
+theme.wallpaper_cmd = { "awsetbg /home/mcampbell/1-data/4-sync/Dropbox/1-data/2-images/backdrops/thematrix.jpg" }
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
