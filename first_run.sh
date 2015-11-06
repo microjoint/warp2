b92d70f9f8b8eb46091e7598c19fe283ec8401a2
Marcus Campbell <marcus.campbell@williamhill.com>
2015-11-06 15:53:17 +0100

    lightdm

---
+++ first_run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt-get install puppet
+#sudo cd /etc/puppet && sudo ./rush.sh work workstation noc
---
+++ modules/profile/manifests/noc.pp
@@ -0,0 +1,8 @@
+# This is for a NOC pc
+class profile::noc(
+  $xrandr = hiera('xrandr', nil) # command to customise screens
+){
+
+  include lightdm
+
+}
