02ca3fb6881c3b6921d769d2f879d5a61e277414
Marcus Campbell <marcus.campbell@williamhill.com>
2015-12-02 16:02:25 +0100

    removed extra comma

--- modules/profile/files/noc/awesome/rc.lua
+++ modules/profile/files/noc/awesome/rc.lua
@@ -245,7 +245,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
     -- Prompt
-    awful.key({ modkey },            "r",       function () mypromptbox[mouse.screen]:run() end),
+    awful.key({ modkey },            "r",       function () mypromptbox[mouse.screen]:run() end)
 
 )
 
